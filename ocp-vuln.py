6fba8879c87d49d46e2ecd959a7a9d63f1fd92a1
Franck Grosjean <fgrosjean@redhat.com>
2024-12-10 15:44:21 +0100

    add --start --end option to list cve between 2 version

--- ocp-vuln.py
+++ ocp-vuln.py
@@ -2,7 +2,6 @@
 
 import requests
 import argparse
-import json
 
 from vex import color
 from vex import Vex
@@ -46,6 +45,34 @@ def get_zstream_from_cve(cve):
     matching_zstream.sort(reverse = True)
     return matching_zstream
 
+def get_cve_from_to(ocp_from, ocp_to=None):
+    matching_cve = []
+    if not ocp_to:
+        ocp_to = ocp_from
+        print(f"{color.GREEN}Red Hat Openshift Container Platform CVE for {ocp_from}{color.END}")
+    else:
+        print(f"{color.GREEN}Red Hat Openshift Container Platform CVE from {ocp_from} to {ocp_to}{color.END}")
+    
+    from_xstream, from_ystream, from_zstream = ocp_from.split('.')
+    to_xstream, to_ystream, to_zstream = ocp_to.split('.')
+    for xstream in range(int(from_xstream), int(to_xstream)+1):
+        for ystream in range(int(from_ystream), int(to_ystream)+1):
+            for zstream in range(int(from_zstream), int(to_zstream)+1):
+                advisory = str(xstream) + '.' + str(ystream) + '.' + str(zstream)
+                for target, advisory in openshift.rhsa[advisory].items():
+                    if advisory != "None" and "RHSA" in advisory:
+                        vex_document = Vex(advisory)
+                        for vulnerability in vex_document.vulnerabilities:
+                            if vulnerability not in matching_cve:
+                                matching_cve.append(vulnerability)
+    for vulnerability in matching_cve:
+        cve = Vex(vulnerability)
+        highlight = color.END
+        if cve.threat_severity == 'Critical' or float(cve.cvss) >= 8.0:
+            highlight = color.RED
+        print(f"    - {cve.id:<20} {highlight}{cve.cvss:>5} {cve.threat_severity:<12}{color.END}|  {cve.title}")
+
+
 def main():
     if args.cve:
         matching_zstream = get_zstream_from_cve(args.cve)
@@ -67,20 +94,23 @@ def main():
                 print(f"    - {cve.id:<20} {highlight}{cve.cvss:>5} {cve.threat_severity:<12}{color.END}|  {cve.title:<}")
         else:
             print(args.rhsa + " not found")
+    elif args.start:
+        get_cve_from_to(args.start, args.end)
     else:
         print("check help")
 
 
 if __name__ == "__main__":
 
-    # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--list", action="store_true", help="list cve by z-stream")
-    parser.add_argument("--cve", type=str, help="find z-stream fixing the cve")
-    parser.add_argument("--rhsa", type=str, help="find cve fixed in a RHSA")
-    parser.add_argument("--cache", action="store_true", help="use data on disk")
-    parser.add_argument("--test", action="store_true", help="test flag")
-    parser.add_argument("--verbose", action="store_true", help="[unused] increase output verbosity")
+    parser.add_argument('--cve', type=str, help="find z-stream fixing the cve")
+    parser.add_argument('--rhsa', type=str, help="find cve fixed in a RHSA")
+    parser.add_argument('--start', type=str, help="from test flag")
+    parser.add_argument('--end', type=str, help="to test flag")
+    parser.add_argument('--list', action="store_true", help="list cve by z-stream")
+    parser.add_argument('--cache', action="store_true", help="use data on disk")
+    parser.add_argument('--test', action="store_true", help="test flag")
+    parser.add_argument('--verbose', action="store_true", help="[unused] increase output verbosity")
     args = parser.parse_args()
 
     main()
